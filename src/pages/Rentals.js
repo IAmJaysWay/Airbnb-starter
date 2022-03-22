ccd09010851b0842c2bfbaa81854d55ff670b6e4
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-03-22 13:52:45 +0200

    Add files via upload

---
+++ src/pages/Rentals.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Rentals.css'
+
+const Rentals = () => {
+  
+  return (
+  <>
+  <div className="container">
+  <Link to="/" className="link">Home</Link>
+  </div>
+  </>
+)
+}
+
+export default Rentals;
